7b1fea11f15d2d29facd2a97880a1ea56e85b3b7
Michael Drogalis <mjd3089@rit.edu>
2015-12-05 11:19:20 -0800

    Bump version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-etl-support "0.8.2.0-SNAPSHOT"
+(defproject org.onyxplatform/onyx-etl-support "0.8.2.0"
   :description "Supporting code for the onyx-etl tool"
   :url "https://github.com/onyx-platform/onyx-etl-support"
   :license {:name "Eclipse Public License"
